4d815ce14d8d1667c096e91bf2ca1d452d2bb5a6
S4nt05 <60900178+S4nt05@users.noreply.github.com>
2025-09-14 20:23:23 -0600

    mesaages lsit

--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -21,7 +21,6 @@ const MessageList: React.FC<{ accessToken?: string }> = ({ accessToken }) => {
 
   const fetchMessages = async () => {
     try {
-      console.log('accessToken : ',accessToken , ' tendioo_access_token : ',localStorage.getItem('tendioo_access_token'));
       const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000'}/api/messages`, {
         params: { access_token: accessToken || localStorage.getItem('tendioo_access_token') }
       });
